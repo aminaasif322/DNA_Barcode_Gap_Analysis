a8c4a9446c569798e43c825fd1cd74fd582d55b2
Amina Asif <73521443+aminaasif322@users.noreply.github.com>
2022-08-06 21:44:17 -0400

    Add files via upload

---
+++ filter_length.R
@@ -0,0 +1,19 @@
+filter_lentgh<-function(fileLoc = NULL, maxLen = 1000){
+  #Get the initial working directory
+  start_wd <- getwd()
+  on.exit(setwd(start_wd))
+  
+  #load in the output table
+  if (is.null(fileLoc)){
+    print(paste0("Select a file"))
+    fileLoc <- file.choose()
+  }
+  setwd(dirname(fileLoc))
+  
+  #Read in the BLAST results for the file in this loop
+  mos <- read.delim(fileLoc, header = TRUE, sep = "\t")
+  
+  mos<-mos[nchar(mos$Sequence) <= maxLen,]
+  write.table(as.data.frame(mos), file = paste0(sub("\\..*","",as.vector(fileLoc)), "_long_seq_reduced.tsv"), row.names = FALSE, col.names=TRUE, append = FALSE, sep="\t", quote = FALSE)
+  
+}
